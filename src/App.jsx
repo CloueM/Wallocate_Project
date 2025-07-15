17dab83c1a4efe6e42b756e093cc60a1a0e9037b
CM <maczyepez@gmail.com>
2025-07-14 17:11:41 -0700

    Create base website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <main>
+        <div>
+            <h1 className="text-main"> Welcome to Wallocate </h1>
+        </div>
+    </main>
+  )
+}
+
+export default App
